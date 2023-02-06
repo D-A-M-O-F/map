38aa9a5a43e676b9b7009fcb9c5e226972b79ae5
Francesco Emanuele D'Agostino <fedagostino@gmail.com>
2023-02-06 20:38:33 +0100

    initial import

---
+++ include/map.h
@@ -0,0 +1,94 @@
+/**************************************************************************************************
+ * 
+ * Copyright 2022 https://github.com/fe-dagostino
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms 
+ * of the GNU Affero General Public License as published by the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program. 
+ * If not, see <https://www.gnu.org/licenses/>
+ *
+ *************************************************************************************************/
+
+#include <ure_application.h>
+#include <ure_resources_fetcher.h>
+#include <ure_window.h>
+#include <ure_view_port.h>
+#include <ure_position.h>
+#include <ure_size.h>
+
+
+class Map : public ure::ApplicationEvents, public ure::WindowEvents, public ure::ResourcesFetcherEvents
+{
+public:
+  /***/
+  Map( int argc, char** argv );
+
+  /***/
+  void run();
+
+  /***/
+  void dispose();
+
+protected:
+  /***/
+  void init( [[__maybe_unused__]] int argc, [[__maybe_unused__]] char** argv );
+  /***/
+  void loadResources();
+  /***/
+  void addCamera();
+  /***/
+  void addLevel0();
+
+// ure::WindowEvents implementation
+protected:
+  /***/
+  virtual ure::void_t  on_mouse_scroll( [[maybe_unused]] ure::Window* pWindow, [[maybe_unused]] ure::double_t dOffsetX, [[maybe_unused]] ure::double_t dOffsetY ) noexcept override;
+
+// ure::ApplicationEvents implementation  
+protected:
+  /***/
+  virtual ure::void_t on_initialize() override;
+
+  /***/
+  virtual ure::void_t on_initialized() override;
+
+  /***/
+  virtual ure::void_t on_finalize() override;
+
+  /***/
+  virtual ure::void_t on_finalized() override;
+
+  virtual ure::void_t on_run() override; 
+
+  /***/
+  virtual ure::void_t on_initialize_error(/* @todo */) override;
+  /***/
+  virtual ure::void_t on_error( [[maybe_unused]] int32_t error, [[maybe_unused]] const std::string& description ) override;
+  /***/
+  virtual ure::void_t on_finalize_error(/* @todo */) override;
+
+// ure::ResourcesFetcherEvents implementation
+protected:  
+  /***/
+  virtual ure::void_t on_download_succeeded( [[maybe_unused]] const std::string& name, [[maybe_unused]] const ure::byte_t* data, [[maybe_unused]] ure::uint_t length ) override;
+  /***/
+  virtual ure::void_t on_download_failed   ( [[maybe_unused]] const std::string& name ) override;
+
+private:
+  bool                        m_bFullScreen;
+  ure::position_t<ure::int_t> m_position;
+  ure::Size                   m_size;
+  ure::Size                   m_fb_size;    // Frame Buffer Size
+
+  ure::Window*                m_pWindow;
+  ure::ViewPort*              m_pViewPort;
+
+  ure::int_t                  m_maxLevels;
+  ure::int_t                  m_curLevel;
+};
+
---
+++ src/main.cpp
@@ -0,0 +1,28 @@
+/**************************************************************************************************
+ * 
+ * Copyright 2022 https://github.com/fe-dagostino
+ * 
+ * This program is free software: you can redistribute it and/or modify it under the terms 
+ * of the GNU Affero General Public License as published by the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+ * See the GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with this program. 
+ * If not, see <https://www.gnu.org/licenses/>
+ *
+ *************************************************************************************************/
+
+#include "map.h"
+
+int main(int argc, char** argv)
+{
+  Map _map(argc, argv);
+
+  _map.run();
+  
+  _map.dispose();
+
+  return 0;
+}
